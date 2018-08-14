3ed923d55325fd130e0419827b35ab987530243c
alstn2468 <alstn2468_@naver.com>
2018-08-14 17:08:44 +0900

    2018-08-14

--- instagram-ui-clone/App.js
+++ instagram-ui-clone/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+// Add AppStackNavigator Module From react-navigation lib
+import {createStackNavigator} from 'react-navigation';
+// Add MainScreen
+import MainScreen from './Components/MainScreen';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <AppStackNavigator/>
+        );
+    }
 }
 
+// Add StackNavigator to App.js
+const AppStackNavigator = createStackNavigator({
+    Main: {
+        screen: MainScreen
+    },
+});
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 });
