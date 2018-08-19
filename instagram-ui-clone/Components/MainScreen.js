734165c10a62d325ec1806b36ee1993618b9a1e0
alstn2468 <alstn2468_@naver.com>
2018-08-19 09:32:44 +0900

    2018-08-19

--- instagram-ui-clone/Components/MainScreen.js
+++ instagram-ui-clone/Components/MainScreen.js
@@ -1,15 +1,45 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-natvie';
+import {View, Text, StyleSheet} from 'react-native';
+import {Icon} from 'native-base';
+import {createTabNavigator} from 'react-navigation';
+
+import HomeTab from './AppTabNavigator/HomeTab';
+import SearchTab from './AppTabNavigator/SearchTab';
+import AddMediaTab from './AppTabNavigator/AddMediaTab';
+import LikesTab from './AppTabNavigator/LikesTab';
+import ProfileTab from './AppTabNavigator/ProfileTab';
 
 class MainScreen extends Component {
+
+    static navigationOptions = {
+        headerLeft: <Icon name='ios-camera-outline' style={{paddingLeft:10}}/>,
+        title: 'Instagram',
+        headerRight: <Icon name='ios-send-outline' style={{paddingRight:10}}/>,
+    }
+
     render() {
-        return (<View style={style.container}>
-            <Text>MainScreen</Text>
-        </View>);
+        return (
+            <AppTabNavigator/>
+        );
     }
 }
 
 export default MainScreen;
+
+const AppTabNavigator = createTabNavigator({
+    HomeTab: {
+        screen: HomeTab
+    },
+    Search: {
+        screen: SearchTab
+    },
+    Likes: {
+        screen: LikesTab
+    },
+    Profile: {
+        screen: ProfileTab
+    }
+})
 
 const style = StyleSheet.create({
     container: {
