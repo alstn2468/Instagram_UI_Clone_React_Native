d25ac53dc348f27c01a1821c8f598c7e95f93846
alstn2468 <alstn2468_@naver.com>
2018-08-24 10:36:35 +0900

    Add Container in ProfileTab.js

--- instagram-ui-clone/Components/AppTabNavigator/ProfileTab.js
+++ instagram-ui-clone/Components/AppTabNavigator/ProfileTab.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import {Icon} from 'native-base';
+import {Icon, Container, Content, Header, Left, Body, Right, BUtton} from 'native-base';
 
 class ProfileTab extends Component {
     static navigationOptions = {
@@ -11,9 +11,19 @@ class ProfileTab extends Component {
 
     render() {
         return (
-            <View style={style.container}>
-                <Text>ProfileTab</Text>
-            </View>
+            <Container>
+                <Header>
+                    <Left style={{flexDirection:'row', alignItems: 'center'}}>
+                        <Text style={{fontWeight:'bold', fontSize: 17}}>_alstn2468_</Text>
+                        <Icon name='caret-down' type='FontAwesome' style={style.leftIcon}/>
+                    </Left>
+                    <Right style={{flexDirection: 'row', alignItems: 'center'}}>
+                        <Icon name='back-in-time' type='Entypo' style={style.rightIcon}/>
+                        <Icon name='user-plus' type='Feather' style={style.rightIcon}/>
+                        <Icon name='dots-vertical' type='MaterialCommunityIcons' style={style.rightIcon}/>
+                    </Right>
+                </Header>
+            </Container>
         );
     }
 }
@@ -25,5 +35,13 @@ const style = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    leftIcon: {
+        paddingLeft: 10,
+        fontSize: 14
+    },
+    rightIcon: {
+        paddingRight: 10,
+        fontSize: 23
     }
 })
